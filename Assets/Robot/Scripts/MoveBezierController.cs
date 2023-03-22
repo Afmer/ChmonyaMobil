2d26c6e3f566548bbbf4af2d7a365a01b184f9b7
Sergey <grevich13@gmail.com>
2023-03-22 19:26:33 +0300

    Add Move in MoveBezierController

--- Assets/Robot/Scripts/MoveBezierController.cs
+++ Assets/Robot/Scripts/MoveBezierController.cs
@@ -8,6 +8,7 @@ namespace OmniRobot
 {
     public class MoveBezierController : MovePoint
     {
+        [SerializeField][Range(0, 1)] private float _step;
         public BezierLine BezierLine
         {
             get
@@ -30,8 +31,39 @@ namespace OmniRobot
         {
             if(Application.isPlaying)
             {
-
+                if (!_isMoving)
+                {
+                    _isMoving = true;
+                    StartCoroutine(MoveCoroutine());
+                }
+                else throw new System.Exception("bezier movement is active");
             }
+        }
+
+        private IEnumerator MoveCoroutine()
+        {
+            for(int i = 0; i < BezierLine.SegmentsNums; i++)
+            {
+                float angle;
+                for(float j = 0; j < 1; j += _step)
+                {
+                    MoveToPoint(BezierLine.GetPointToSegmentIndex(i, j));
+                    while(IsMovingToPoint)
+                        yield return null;
+                    angle = BezierLine.GetRotationToSegmentIndex(i, j).y - transform.eulerAngles.y;
+                    _movementLogic.Rotate(angle);
+                    while(_movementLogic.IsRotationCoroutineActive)
+                        yield return null;
+                }
+                MoveToPoint(BezierLine.GetPointToSegmentIndex(i, 1));
+                while (IsMovingToPoint)
+                    yield return null;
+                angle = BezierLine.GetRotationToSegmentIndex(i, 1).y - transform.eulerAngles.y;
+                _movementLogic.Rotate(angle);
+                while (_movementLogic.IsRotationCoroutineActive)
+                    yield return null;
+            }
+            _isMoving = false;
         }
     }
 }
