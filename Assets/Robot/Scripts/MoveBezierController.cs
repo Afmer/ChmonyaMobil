b4f2882cf6e3fe0e81f74f812beb3d90a04ae666
Afmer <grevich13@gmail.com>
2023-03-23 10:34:25 +0300

    Delete extra rotation in MoveBezierController

--- Assets/Robot/Scripts/MoveBezierController.cs
+++ Assets/Robot/Scripts/MoveBezierController.cs
@@ -44,35 +44,14 @@ namespace OmniRobot
         {
             for(int i = 0; i < BezierLine.SegmentsNums; i++)
             {
-                float angle, bezierAngle, transformAngle;
                 for(float j = 0; j < 1; j += _step)
                 {
                     MoveToPoint(BezierLine.GetPointToSegmentIndex(i, j));
                     while(IsMovingToPoint)
                         yield return null;
-                    bezierAngle = Quaternion.LookRotation(BezierLine.GetRotationToSegmentIndex(i, j)).eulerAngles.y;
-                    if (bezierAngle > 180)
-                        bezierAngle -= 360;
-                    transformAngle = transform.eulerAngles.y;
-                    if (transformAngle > 180)
-                        transformAngle -= 360;
-                    angle = bezierAngle - transformAngle;
-                    _movementLogic.Rotate(angle);
-                    while(_movementLogic.IsRotationCoroutineActive)
-                        yield return null;
                 }
                 MoveToPoint(BezierLine.GetPointToSegmentIndex(i, 1));
                 while (IsMovingToPoint)
-                    yield return null;
-                bezierAngle = Quaternion.LookRotation(BezierLine.GetRotationToSegmentIndex(i, 1)).eulerAngles.y;
-                if (bezierAngle > 180)
-                    bezierAngle -= 360;
-                transformAngle = transform.eulerAngles.y;
-                if (transformAngle > 180)
-                    transformAngle -= 360;
-                angle = bezierAngle - transformAngle;
-                _movementLogic.Rotate(angle);
-                while (_movementLogic.IsRotationCoroutineActive)
                     yield return null;
             }
             _isMoving = false;
